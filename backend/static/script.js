ebec38c9002f09c9024a3e275a3372bb590b079b
LuisCorrales05 <139280844+LuisCorrales05@users.noreply.github.com>
2024-07-25 00:20:09 -0600

    Noticias realizadas

--- backend/static/script.js
+++ backend/static/script.js
@@ -24,3 +24,56 @@ var map = L.map('map').setView([9.934739, -84.087502], 13);
         }).addTo(map);
 
 
+async function fetchEarthquakeData() {
+    try {
+        const response = await fetch('http://127.0.0.1:5000/poll');
+                        
+        if (!response.ok) {
+            if (response.status === 404) {
+                console.error('Endpoint not found: 404 error');
+            } else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            } else {
+                const data = await response.json();
+                console.log(data);
+                updateTable(data);
+            }
+    } catch (error) {
+        console.error('Error fetching earthquake data:', error);
+    }
+}
+        
+function updateTable(data) {
+    const tableBody = document.querySelector('.News tbody');
+    tableBody.innerHTML = ''; // Limpiar la tabla antes de actualizar
+        
+    data.forEach(earthquake => {
+        const row = document.createElement('tr');
+        
+        const magnitudeCell = document.createElement('td');
+        magnitudeCell.textContent = earthquake.magnitude;
+        row.appendChild(magnitudeCell);
+        
+        const placeCell = document.createElement('td');
+        placeCell.textContent = earthquake.place;
+        row.appendChild(placeCell);
+        
+        const timeCell = document.createElement('td');
+        const date = new Date(earthquake.time);
+        timeCell.textContent = date.toLocaleString();
+        row.appendChild(timeCell);
+        
+        const longitudeCell = document.createElement('td');
+        longitudeCell.textContent = earthquake.longitude;
+        row.appendChild(longitudeCell);
+        
+        const latitudeCell = document.createElement('td');
+        latitudeCell.textContent = earthquake.latitude;
+        row.appendChild(latitudeCell);
+        
+        tableBody.appendChild(row);
+    });
+}
+        
+document.addEventListener('DOMContentLoaded', fetchEarthquakeData);
