467bad0fbae6429c2d8d21f3023e7002ab00a414
Gabriel Céspedes Ugalde <gcesu0125@gmail.com>
2024-08-19 20:02:19 -0600

    Historial final

--- backend/auth.py
+++ backend/auth.py
@@ -26,8 +26,6 @@ def signPage():
             confirmPass = request.form.get('register-confirmPassword')
             dateOfBirth = request.form.get('register-dateOfBirth')
             
-            
-            
             if password != confirmPass:  # Si las contraseñas no coinciden
                 flash('Las contraseñas no coinciden, por favor intenta de nuevo', category='error')
             else:
@@ -67,19 +65,20 @@ def signPage():
             loginEmail = request.form.get('login-email')
             loginPass = request.form.get('login-password')
             
-            sql_command = text("""
-                SELECT contrasena FROM users WHERE email = :email
-            """)
-            
-            
             # Comando SQL para obtener el usuario por correo electrónico
-            
             user = session.query(User).filter_by(email=loginEmail).first()
             
             if user:  # Si se encontró el usuario
                 stored_password = user.contrasena  # Obtener la contraseña almacenada
                 if check_password_hash(stored_password, loginPass):  # Verificar la contraseña
-                    flash('Inicio de sesion exitoso!', category='success')
+                    # Insertar en la tabla Historial que el usuario está conectado
+                    sql_command = text("""
+                        EXEC sp_InsertarHistorial @idUser=:idUser, @estado=1;
+                    """)
+                    session.execute(sql_command, {'idUser': user.idUser})
+                    session.commit()
+
+                    flash('Inicio de sesión exitoso!', category='success')
                     login_user(user, remember=True)
                     return redirect(url_for('views.home'))  # Redirigir a la página principal
                 else:
@@ -93,8 +92,14 @@ def signPage():
 @auth.route('/logout')
 @login_required
 def logout():
+    # Insertar en la tabla Historial que el usuario se ha desconectado
+    sql_command = text("""
+        EXEC sp_InsertarHistorial @idUser=:idUser, @estado=0;
+    """)
+    session.execute(sql_command, {'idUser': current_user.idUser})
+    session.commit()
+
     logout_user()
-    
     return redirect(url_for('auth.signPage'))  # Redirigir a la página de inicio de sesión y registro
 
 def load_earthquake_data():
--- backend/static/script.js
+++ backend/static/script.js
@@ -1,126 +1,137 @@
 //Script para cambiar de tema en la página web
-document.addEventListener('DOMContentLoaded', function () {
-    const themeButtons = document.querySelectorAll('.dropdown-item[data-theme]');
-    const themeLink = document.getElementById('theme-link');
-    themeButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const theme = this.getAttribute('data-theme');
-            if (theme === 'light') {
-                themeLink.href = 'https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css';
-                document.body.removeAttribute('data-bs-theme');
-            } else if (theme === 'dark') {
-                themeLink.href = 'https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css';
-                document.body.setAttribute('data-bs-theme', 'dark');;
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const themeButtons = document.querySelectorAll(".dropdown-item[data-theme]");
+  const themeLink = document.getElementById("theme-link");
+  themeButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const theme = this.getAttribute("data-theme");
+      if (theme === "light") {
+        themeLink.href =
+          "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css";
+        document.body.removeAttribute("data-bs-theme");
+      } else if (theme === "dark") {
+        themeLink.href =
+          "https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css";
+        document.body.setAttribute("data-bs-theme", "dark");
+      }
     });
+  });
 
-    var userInfoElement = document.querySelector('.user-info');
-    var userNameElement = document.getElementById('user-name');
-    var userName = userInfoElement.getAttribute('data-username');
-    if (userNameElement) {
-        userNameElement.textContent = userName;
-    }
+  var userInfoElement = document.querySelector(".user-info");
+  var userNameElement = document.getElementById("user-name");
+  var userName = userInfoElement.getAttribute("data-username");
+  if (userNameElement) {
+    userNameElement.textContent = userName;
+  }
 
-    var updateNameInput = document.getElementById('update_name');
-    var updateLastNameInput = document.getElementById('update_lastName');
-    var updatePasswordInput = document.getElementById('update_password');
-    var updateDateInput = document.getElementById('update_date');
+  var updateNameInput = document.getElementById("update_name");
+  var updateLastNameInput = document.getElementById("update_lastName");
+  var updatePasswordInput = document.getElementById("update_password");
+  var updateDateInput = document.getElementById("update_date");
 
-    updateNameInput.value = userInfoElement.getAttribute('data-username');
-    updateLastNameInput.value = userInfoElement.getAttribute('data-lastname');
-    updatePasswordInput.value = userInfoElement.getAttribute('data-password');
-    updateDateInput.value = userInfoElement.getAttribute('data-birthdate');
+  updateNameInput.value = userInfoElement.getAttribute("data-username");
+  updateLastNameInput.value = userInfoElement.getAttribute("data-lastname");
+  updatePasswordInput.value = userInfoElement.getAttribute("data-password");
+  updateDateInput.value = userInfoElement.getAttribute("data-birthdate");
 });
 
-let subMenu = document.getElementById('subMenu');
+let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
-    subMenu.classList.toggle('open-menu');
+  subMenu.classList.toggle("open-menu");
 }
 
-
-var map = L.map('map').setView([0, 0], 2);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
-
+var map = L.map("map").setView([0, 0], 2);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap contributors",
+}).addTo(map);
 
 async function fetchEarthquakeData() {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/poll');
-                        
-        if (!response.ok) {
-            if (response.status === 404) {
-                console.error('Endpoint not found: 404 error');
-            } else {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            } else {
-                const data = await response.json();
-                console.log(data);
-                updateTable(data);
-            }
-    } catch (error) {
-        console.error('Error fetching earthquake data:', error);
-    }
-}
-        
-function updateTable(data) {
-    const tableBody = document.querySelector('.News tbody');
-    tableBody.innerHTML = ''; // Limpiar la tabla antes de actualizar
-        
-    data.forEach(earthquake => {
-        const row = document.createElement('tr');
-        
-        const magnitudeCell = document.createElement('td');
-        magnitudeCell.textContent = earthquake.magnitude;
-        row.appendChild(magnitudeCell);
-        
-        const placeCell = document.createElement('td');
-        placeCell.textContent = earthquake.place;
-        row.appendChild(placeCell);
-        
-        const timeCell = document.createElement('td');
-        const date = new Date(earthquake.time);
-        timeCell.textContent = date.toLocaleString();
-        row.appendChild(timeCell);
-        
-        const longitudeCell = document.createElement('td');
-        longitudeCell.textContent = earthquake.longitude;
-        row.appendChild(longitudeCell);
-        
-        const latitudeCell = document.createElement('td');
-        latitudeCell.textContent = earthquake.latitude;
-        row.appendChild(latitudeCell);
-        
-        tableBody.appendChild(row);
+  try {
+    const response = await fetch("http://127.0.0.1:5000/poll");
 
-        L.marker([earthquake.latitude, earthquake.longitude]).addTo(map)
-        .bindPopup('Terremoto de magnitud ' + earthquake.magnitude + ' en ' + earthquake.place + ' el ' + date.toLocaleString())
-        .openPopup();
-    });
+    if (!response.ok) {
+      if (response.status === 404) {
+        console.error("Endpoint not found: 404 error");
+      } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } else {
+      const data = await response.json();
+      console.log(data);
+      updateTable(data);
+    }
+  } catch (error) {
+    console.error("Error fetching earthquake data:", error);
+  }
 }
-        
-document.addEventListener('DOMContentLoaded', fetchEarthquakeData);
+
+function updateTable(data) {
+  const tableBody = document.querySelector(".News tbody");
+  tableBody.innerHTML = ""; // Limpiar la tabla antes de actualizar
+
+  data.forEach((earthquake) => {
+    const row = document.createElement("tr");
+
+    const magnitudeCell = document.createElement("td");
+    magnitudeCell.textContent = earthquake.magnitude;
+    row.appendChild(magnitudeCell);
+
+    const placeCell = document.createElement("td");
+    placeCell.textContent = earthquake.place;
+    row.appendChild(placeCell);
+
+    const timeCell = document.createElement("td");
+    const date = new Date(earthquake.time);
+    timeCell.textContent = date.toLocaleString();
+    row.appendChild(timeCell);
+
+    const longitudeCell = document.createElement("td");
+    longitudeCell.textContent = earthquake.longitude;
+    row.appendChild(longitudeCell);
+
+    const latitudeCell = document.createElement("td");
+    latitudeCell.textContent = earthquake.latitude;
+    row.appendChild(latitudeCell);
+
+    tableBody.appendChild(row);
+
+    L.marker([earthquake.latitude, earthquake.longitude])
+      .addTo(map)
+      .bindPopup(
+        "Terremoto de magnitud " +
+          earthquake.magnitude +
+          " en " +
+          earthquake.place +
+          " el " +
+          date.toLocaleString()
+      )
+      .openPopup();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", fetchEarthquakeData);
 
 const showUpdateModal = () => {
-    const modalWrp = document.querySelector(".modal-update-wrp");
-    modalWrp.classList.remove("invisible");
+  const modalWrp = document.querySelector(".modal-update-wrp");
+  modalWrp.classList.remove("invisible");
 };
 
 function closeUpdateModal() {
-    const modalWrp = document.querySelector(".modal-update-wrp");
-    modalWrp.classList.add("invisible");
+  const modalWrp = document.querySelector(".modal-update-wrp");
+  modalWrp.classList.add("invisible");
 }
 
 function showProfilePicModal() {
-    const modalWrp = document.querySelector(".modal-upload-pic-wrp");
-    modalWrp.classList.remove("invisible");
+  const modalWrp = document.querySelector(".modal-upload-pic-wrp");
+  modalWrp.classList.remove("invisible");
 }
 
 function closeProfilePicModal() {
-    const modalWrp = document.querySelector(".modal-upload-pic-wrp");
-    modalWrp.classList.add("invisible");
-
+  const modalWrp = document.querySelector(".modal-upload-pic-wrp");
+  modalWrp.classList.add("invisible");
 }
+
+window.addEventListener("beforeunload", function () {
+  navigator.sendBeacon("/logout");
+});
