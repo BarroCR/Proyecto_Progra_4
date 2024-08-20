11838c9a8971b602b8acd950c740e3109be502ca
Gabriel Céspedes Ugalde <gcesu0125@gmail.com>
2024-08-19 19:55:50 -0600

    Revert "Manejo de historial de sesiones"
    This reverts commit 0f93de0d5aeffd344cb0617c6c6fde0d4378ae7e.

--- backend/auth.py
+++ backend/auth.py
@@ -26,6 +26,8 @@ def signPage():
             confirmPass = request.form.get('register-confirmPassword')
             dateOfBirth = request.form.get('register-dateOfBirth')
             
+            
+            
             if password != confirmPass:  # Si las contraseñas no coinciden
                 flash('Las contraseñas no coinciden, por favor intenta de nuevo', category='error')
             else:
@@ -65,20 +67,19 @@ def signPage():
             loginEmail = request.form.get('login-email')
             loginPass = request.form.get('login-password')
             
+            sql_command = text("""
+                SELECT contrasena FROM users WHERE email = :email
+            """)
+            
+            
             # Comando SQL para obtener el usuario por correo electrónico
+            
             user = session.query(User).filter_by(email=loginEmail).first()
             
             if user:  # Si se encontró el usuario
                 stored_password = user.contrasena  # Obtener la contraseña almacenada
                 if check_password_hash(stored_password, loginPass):  # Verificar la contraseña
-                    # Insertar en la tabla Historial que el usuario está conectado
-                    sql_command = text("""
-                        EXEC sp_InsertarHistorial @idUser=:idUser, @estado=1;
-                    """)
-                    session.execute(sql_command, {'idUser': user.idUser})
-                    session.commit()
-
-                    flash('Inicio de sesión exitoso!', category='success')
+                    flash('Inicio de sesion exitoso!', category='success')
                     login_user(user, remember=True)
                     return redirect(url_for('views.home'))  # Redirigir a la página principal
                 else:
@@ -92,14 +93,8 @@ def signPage():
 @auth.route('/logout')
 @login_required
 def logout():
-    # Insertar en la tabla Historial que el usuario se ha desconectado
-    sql_command = text("""
-        EXEC sp_InsertarHistorial @idUser=:idUser, @estado=0;
-    """)
-    session.execute(sql_command, {'idUser': current_user.idUser})
-    session.commit()
-
     logout_user()
+    
     return redirect(url_for('auth.signPage'))  # Redirigir a la página de inicio de sesión y registro
 
 def load_earthquake_data():
