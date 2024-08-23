f34165a7c1f8740e05a42b16b5e909d995447c19
LuisCorrales05 <139280844+LuisCorrales05@users.noreply.github.com>
2024-08-23 02:04:55 -0600

    Simulacion de seccion de noticias

--- backend/static/app.js
+++ backend/static/app.js
@@ -1,93 +1,97 @@
+var userInfoElement = document.querySelector('.reply-input');
+var userName = userInfoElement.getAttribute('data-username');
+var userImageElement = document.querySelector('.usr-img');
+var userImageSrc = userImageElement
+  ? userImageElement.getAttribute('src')
+  : '/static/images/icons8-user-64.png';
+
+console.log(userImageSrc);
+
 const data = {
-  currentUser: {
-    image: {
-      png: "./images/avatars/default_avatar.png",
-      webp: "./images/avatars/defaultWEB_avatar.webp",
-    },
-    username: "Luis",
-  },
-  comments: [
-    {
-      parent: 0,
-      id: 1,
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nobis suscipit officia ea corporis consequatur dolores quod, aliquam pariatur veniam, quae enim aspernatur inventore. Nemo repudiandae neque nesciunt adipisci ab!",
-      createdAt: "1 month ago",
-      user: {
+    currentUser: {
         image: {
-          png: "./images/avatars/default_avatar.png",
-          webp: "./images/avatars/defaultWEB_avatar.webp",
+            png: userImageSrc,
+            webp: userImageSrc,
         },
-        username: "Brian",
-      },
-      replies: [],
+        username: userName,
     },
-    {
-      /// c:
-      parent: 0,
-      id: 2,
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nobis suscipit officia ea corporis consequatur dolores quod, aliquam pariatur veniam, quae enim aspernatur inventore. Nemo repudiandae neque nesciunt adipisci ab!",
-      createdAt: "2 weeks ago",
-      user: {
-        image: {
-          png: "./images/avatars/default_avatar.png",
-          webp: "./images/avatars/defaultWEB_avatar.webp",
-        },
-        username: "Gabriel",
-      },
-      replies: [
+    comments: [
         {
-          parent: 2,
-          id: 1,
-          content:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nobis suscipit officia ea corporis",
-          createdAt: "1 week ago",
-          replyingTo: "Gabriel",
-          user: {
-            image: {
-              png: "./images/avatars/default_avatar.png",
-              webp: "./images/avatars/defaultWEB_avatar.webp",
+            parent: 0,
+            id: 1,
+            content: 
+                "¡Alerta sísmica! Un terremoto de magnitud 6.2 ha sacudido la región de San Salvador. Se reportan daños en varios edificios y se están realizando evacuaciones. Manténgase a salvo y siga las indicaciones de las autoridades locales.",
+            createdAt: "Hace 1 mes",
+            user: {
+                image: {
+                    png: "/static/images/icons8-user-64.png",
+                    webp: "/static/images/icons8-user-64.png",
+                },
+                username: "Brian",
             },
-            username: "Kendal",
-          },
+            replies: [],
         },
         {
-          parent: 2,
-          id: 1,
-          content:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione nobis suscipit officia ea corporis consequatur dolores quod, aliquam pariatur",
-          createdAt: "2 days ago",
-          replyingTo: "Kendal",
-          user: {
-            image: {
-              png: "./images/avatars/default_avatar.png",
-              webp: "./images/avatars/defaultWEB_avatar.webp",
+            parent: 0,
+            id: 2,
+            content:
+                "Nuevo informe sobre el terremoto en la región de México: La magnitud se ha ajustado a 7.1 y se han confirmado varias víctimas y heridos. Las autoridades están trabajando en el rescate de personas atrapadas entre los escombros. Se recomienda a la población que evite las áreas afectadas.",
+            createdAt: "Hace 2 semanas",
+            user: {
+                image: {
+                    png: "/static/uploads/Imagen_de_WhatsApp_2024-07-20_a_las_14.30.46_c21c79c0.jpg",
+                    webp: "/static/uploads/Imagen_de_WhatsApp_2024-07-20_a_las_14.30.46_c21c79c0.jpg",
+                },
+                username: "Gabriel",
             },
-            username: "Luis",
-          },
+            replies: [
+                {
+                    parent: 2,
+                    id: 1,
+                    content:
+                        "He visto las imágenes y es devastador. ¿Hay alguna información sobre cómo podemos ayudar desde fuera? Me gustaría saber cómo hacer una donación o qué tipo de ayuda es necesaria.",
+                    createdAt: "Hace 1 semana",
+                    replyingTo: "Gabriel",
+                    user: {
+                        image: {
+                            png: "/static/uploads/Mcqueen.jpg",
+                            webp: "/static/uploads/Mcqueen.jpg",
+                        },
+                        username: "Kendal",
+                    },
+                },
+                {
+                    parent: 2,
+                    id: 2,
+                    content:
+                        "En las noticias dijeron que la mayoría de los daños están en áreas residenciales. Me preocupa la situación de los afectados. ¿Alguien tiene información actualizada sobre los esfuerzos de rescate?",
+                    createdAt: "Hace 2 días",
+                    replyingTo: "Kendal",
+                    user: {
+                        image: {
+                            png: "/static/uploads/channels4_profile.jpg",
+                            webp: "/static/uploads/channels4_profile.jpg",
+                        },
+                        username: "Luis",
+                    },
+                },
+            ],
         },
-      ],
-    },
-  ],
+    ],
 };
 
-// Función para agregar un fragmento a un elemento padre y devolver el hijo agregado
+
 function appendFrag(frag, parent) {
   var children = [].slice.call(frag.childNodes, 0);
   parent.appendChild(frag);
   return children[1];
 }
 
-// Función para agregar un comentario al objeto de datos
 const addComment = (body, parentId, replyTo = undefined) => {
-  // Determinar el comentario padre en función del parentId
   let commentParent =
     parentId === 0
       ? data.comments
       : data.comments.filter((c) => c.id == parentId)[0].replies;
-
-  // Crear un nuevo objeto de comentario
   let newComment = {
     parent: parentId,
     id:
@@ -95,88 +99,87 @@ const addComment = (body, parentId, replyTo = undefined) => {
         ? 1
         : commentParent[commentParent.length - 1].id + 1,
     content: body,
-    createdAt: "Ahora",
+    createdAt: 'Now',
     replyingTo: replyTo,
-    replies: parentId == 0 ? [] : undefined,
+    replies: parent == 0 ? [] : undefined,
     user: data.currentUser,
   };
-
-  // Agregar el nuevo comentario al arreglo commentParent
   commentParent.push(newComment);
-
-  // Volver a inicializar los comentarios
   initComments();
 };
-
-// Función para eliminar un comentario del objeto de datos
 const deleteComment = (commentObject) => {
   if (commentObject.parent == 0) {
-    // Si el comentario es un comentario de nivel superior, eliminarlo del arreglo de comentarios
     data.comments = data.comments.filter((e) => e != commentObject);
   } else {
-    // Si el comentario es una respuesta, eliminarlo del arreglo de respuestas de su comentario padre
     data.comments.filter((e) => e.id === commentObject.parent)[0].replies =
       data.comments
         .filter((e) => e.id === commentObject.parent)[0]
         .replies.filter((e) => e != commentObject);
   }
-
-  // Volver a inicializar los comentarios
   initComments();
 };
 
-// Función para solicitar la eliminación de un comentario
 const promptDel = (commentObject) => {
-  const modalWrp = document.querySelector(".modal-wrp");
-  modalWrp.classList.remove("invisible");
-  modalWrp.querySelector(".yes").addEventListener("click", () => {
+  const modalWrp = document.querySelector('.modal-wrp');
+  modalWrp.classList.remove('invisible');
+  modalWrp.querySelector('.yes').addEventListener('click', () => {
     deleteComment(commentObject);
-    modalWrp.classList.add("invisible");
+    modalWrp.classList.add('invisible');
   });
-  modalWrp.querySelector(".no").addEventListener("click", () => {
-    modalWrp.classList.add("invisible");
+  modalWrp.querySelector('.no').addEventListener('click', () => {
+    modalWrp.classList.add('invisible');
   });
 };
 
-// Función para crear un nodo de comentario
-const createCommentNode = (commentObject) => {
-  // Clonar la plantilla de comentario y llenarla con los datos del objeto de comentario
-  const commentTemplate = document.querySelector(".comment-template");
-  var commentNode = commentTemplate.content.cloneNode(true);
-  commentNode.querySelector(".usr-name").textContent =
-    commentObject.user.username;
-  commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
-  commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
-  commentNode.querySelector(".c-body").textContent = commentObject.content;
-  if (commentObject.replyingTo)
-    commentNode.querySelector(".reply-to").textContent =
-      "@" + commentObject.replyingTo;
+const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+  if (parent.querySelectorAll('.reply-input')) {
+    parent.querySelectorAll('.reply-input').forEach((e) => {
+      e.remove();
+    });
+  }
+  const inputTemplate = document.querySelector('.reply-input-template');
+  const inputNode = inputTemplate.content.cloneNode(true);
+  const addedInput = appendFrag(inputNode, parent);
+  addedInput.querySelector('.bu-primary').addEventListener('click', () => {
+    let commentBody = addedInput.querySelector('.cmnt-input').value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, parentId, replyTo);
+  });
+};
 
-  // Agregar un event listener al botón "Eliminar" si el comentario pertenece al usuario actual
-  if (commentObject.user.username == data.currentUser.username) {
-    commentNode.querySelector(".comment").classList.add("this-user");
-    commentNode.querySelector(".delete").addEventListener("click", () => {
+const createCommentNode = (commentObject) => {
+  const commentTemplate = document.querySelector('.comment-template');
+  var commentNode = commentTemplate.content.cloneNode(true);
+  commentNode.querySelector('.usr-name').textContent =
+    commentObject.user.username;
+  commentNode.querySelector('.usr-img').src = commentObject.user.image.webp;
+
+  commentNode.querySelector('.cmnt-at').textContent = commentObject.createdAt;
+  commentNode.querySelector('.c-body').textContent = commentObject.content;
+  if (commentObject.replyingTo)
+    commentNode.querySelector('.reply-to').textContent =
+      '@' + commentObject.replyingTo;
+
+  if (commentObject.user.username == data.currentUser.username || data.currentUser.username === 'Admin') {
+    commentNode.querySelector('.comment').classList.add('this-user');
+    commentNode.querySelector('.delete').addEventListener('click', () => {
       promptDel(commentObject);
     });
   }
-
   return commentNode;
 };
 
-// Función para agregar un nodo de comentario a su elemento padre
 const appendComment = (parentNode, commentNode, parentId) => {
-  const bu_reply = commentNode.querySelector(".reply");
+  const bu_reply = commentNode.querySelector('.reply');
 
   const appendedCmnt = appendFrag(commentNode, parentNode);
-  const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
-
-  // Agregar un event listener al botón "Responder"
-  bu_reply.addEventListener("click", () => {
-    if (parentNode.classList.contains("replies")) {
+  const replyTo = appendedCmnt.querySelector('.usr-name').textContent;
+  bu_reply.addEventListener('click', () => {
+    if (parentNode.classList.contains('replies')) {
       spawnReplyInput(parentNode, parentId, replyTo);
     } else {
       spawnReplyInput(
-        appendedCmnt.querySelector(".replies"),
+        appendedCmnt.querySelector('.replies'),
         parentId,
         replyTo
       );
@@ -184,37 +187,26 @@ const appendComment = (parentNode, commentNode, parentId) => {
   });
 };
 
-// Función para inicializar los comentarios
 function initComments(
   commentList = data.comments,
-  parent = document.querySelector(".comments-wrp")
+  parent = document.querySelector('.comments-wrp')
 ) {
-  // Limpiar el elemento padre
-  parent.innerHTML = "";
-
-  // Iterar a través de commentList y crear nodos de comentario
+  parent.innerHTML = '';
   commentList.forEach((element) => {
     var parentId = element.parent == 0 ? element.id : element.parent;
     const comment_node = createCommentNode(element);
-
-    // Si el comentario tiene respuestas, inicializar recursivamente las respuestas
     if (element.replies && element.replies.length > 0) {
-      initComments(element.replies, comment_node.querySelector(".replies"));
+      initComments(element.replies, comment_node.querySelector('.replies'));
     }
-
-    // Agregar el nodo de comentario al elemento padre
     appendComment(parent, comment_node, parentId);
   });
 }
 
-// Inicializar los comentarios
 initComments();
-
-// Agregar un event listener al botón "Agregar Comentario"
-const cmntInput = document.querySelector(".reply-input");
-cmntInput.querySelector(".bu-primary").addEventListener("click", () => {
-  let commentBody = cmntInput.querySelector(".cmnt-input").value;
+const cmntInput = document.querySelector('.reply-input');
+cmntInput.querySelector('.bu-primary').addEventListener('click', () => {
+  let commentBody = cmntInput.querySelector('.cmnt-input').value;
   if (commentBody.length == 0) return;
   addComment(commentBody, 0);
-  cmntInput.querySelector(".cmnt-input").value = "";
+  cmntInput.querySelector('.cmnt-input').value = '';
 });
